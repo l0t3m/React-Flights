a4fc0e5590d12ffec3b24790e61bbef842789ea5
lotem <lotem1shriker@gmail.com>
2023-09-12 21:28:59 +0300

    End lesson 8

--- static/api.js
+++ static/api.js
@@ -1,5 +1,7 @@
 // JavaScript:
 
+// https://data.gov.il/api/3/action/datastore_search?resource_id=e83f763b-b7d7-479e-b172-ae981ddc6de5&limit=5
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function ShowFlights(props) {
@@ -7,7 +9,7 @@ function ShowFlights(props) {
 
     React.useEffect(() => {
         axios.get('https://data.gov.il/api/3/action/datastore_search?resource_id=e83f763b-b7d7-479e-b172-ae981ddc6de5&limit=5').then( (response) => {
-            setRecords(response.json.result.records)
+            setRecords(response.data.result.records)
         })
     })
 
@@ -15,9 +17,29 @@ function ShowFlights(props) {
         <div>
             <div>records len: {records.length}</div>
             <div>typeof records: {typeof(records)}</div>
+
+            <br/>
             
-            <div>
-            {records.map( (record) => <div> {record._id} </div>)}
+            <div className="table">
+                <div className="row">
+                    <div className='header'>Flight Id:</div>
+                    {records.map( (record) => <div>{record._id} </div>)}
+                </div>
+
+                <div className="row">
+                    <div className='header'>Time of taking off:</div>
+                    {records.map( (record) => <div>{record.CHSTOL} </div>)}
+                </div>
+
+                <div className="row">
+                    <div className='header'>Time of landing:</div>
+                    {records.map( (record) => <div>{record.CHPTOL} </div>)}
+                </div>
+
+                <div className="row">
+                    <div className='header'>Airplane's Airline:</div>
+                    {records.map( (record) => <div>{record.CHOPERD} </div>)}
+                </div>
             </div>
 
         </div>
