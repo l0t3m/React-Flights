e73195394722feead89fa9b2b7da6f6e02faf790
lotem <lotem1shriker@gmail.com>
2023-09-15 15:41:47 +0300

    Table is done

--- static/api.js
+++ static/api.js
@@ -8,40 +8,37 @@ function ShowFlights(props) {
     const [records, setRecords] = React.useState([])
 
     React.useEffect(() => {
-        axios.get('https://data.gov.il/api/3/action/datastore_search?resource_id=e83f763b-b7d7-479e-b172-ae981ddc6de5&limit=5').then( (response) => {
+        axios.get('https://data.gov.il/api/3/action/datastore_search?resource_id=e83f763b-b7d7-479e-b172-ae981ddc6de5&limit=10').then( (response) => {
             setRecords(response.data.result.records)
         })
     })
 
     return (
         <div>
-            <div>records len: {records.length}</div>
-            <div>typeof records: {typeof(records)}</div>
-
-            <br/>
-            
-            <div className="table">
-                <div className="row">
-                    <div className='header'>Flight Id:</div>
-                    {records.map( (record) => <div>{record._id} </div>)}
-                </div>
-
-                <div className="row">
-                    <div className='header'>Time of taking off:</div>
-                    {records.map( (record) => <div>{record.CHSTOL} </div>)}
-                </div>
-
-                <div className="row">
-                    <div className='header'>Time of landing:</div>
-                    {records.map( (record) => <div>{record.CHPTOL} </div>)}
-                </div>
-
-                <div className="row">
-                    <div className='header'>Airplane's Airline:</div>
-                    {records.map( (record) => <div>{record.CHOPERD} </div>)}
-                </div>
+            <div className="headers">
+                <div className="bigHeader">Ben Gurion Airport</div>
+                <div className="header">Flights:</div>
             </div>
 
+            <div className="divTable">
+                <div className="headRow container">
+                    <div className="divCell">Flight Id</div>
+                    <div className="divCell">Departure</div>
+                    <div className="divCell">Airplane's Airline</div>
+                    <div className="divCell">Terminal</div>
+                    <div className="divCell">Destination</div>
+                </div>
+
+                {records.map( (record) => 
+                    <div className="divRow container">
+                        <div className="divCell"> {record.CHFLTN}</div>
+                        <div className="divCell">{record.CHSTOL}</div>
+                        <div className="divCell">{record.CHOPERD}</div>
+                        <div className="divCell">{record.CHTERM}</div>
+                        <div className="divCell">{record.CHLOCCT} {record.CHLOC1T}</div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
